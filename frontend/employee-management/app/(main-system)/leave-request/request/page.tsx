25fed0438f1a3c9014856542bf33b735b63cfcd6
night-sornram <6633236021@student.chula.ac.th>
2024-05-16 18:18:13 +0700

    Edit Leave request

--- frontend/employee-management/app/(main-system)/leave-request/request/page.tsx
+++ frontend/employee-management/app/(main-system)/leave-request/request/page.tsx
@@ -1,6 +1,13 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+import DatePickerRange from "@/components/Shadcnui/DatePickerRange";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import dayjs from "dayjs";
@@ -10,17 +17,28 @@ export default function LeaveRequestPage () {
     const current = dayjs().format('DD/MM/YYYY');
 
     return (
-        <div className="flex flex-col px-[10%] py-[5%] w-[60%] gap-[10%]">
+        <div className="flex flex-col px-[10%] py-[5%] w-[60%] gap-[5%]">
             <h1 className="text-2xl font-bold">Leave Request</h1>
-            <div className="flex flex-row justify-between w-full">
-                <div>
-                    <Label htmlFor="from">From (first day of leave)</Label>
-                    <Input type="date" id="from" className="mt-3" required={true}/>  
-                </div>
-                <div>
-                    <Label htmlFor="to">To (last day of leave)</Label>
-                    <Input type="date" id="to" className="mt-3" required={true}/>  
-                </div>
+            <div className="flex flex-col space-y-3 justify-between w-full">
+                <Label htmlFor="reason">Leave time</Label>
+                <DatePickerRange className="w-full"/>
+            </div>
+            <div className=" flex flex-col space-y-3">
+                <Label htmlFor="reason">Category of Leave</Label>
+                <Select>
+                    <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Category" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="annual_leave">Annual leave</SelectItem>
+                        <SelectItem value="maternity_leave">Maternity leave</SelectItem>
+                        <SelectItem value="bereavement">Bereavement</SelectItem>
+                        <SelectItem value="sick_leave">Sick leave</SelectItem>
+                        <SelectItem value="parental_leave">Parental leave</SelectItem>
+                        <SelectItem value="unpaid_leave">Unpaid leave</SelectItem>
+                    </SelectContent>
+                </Select>
+
             </div>
             <div>
                 <Label htmlFor="reason">Reason of Leave</Label>
