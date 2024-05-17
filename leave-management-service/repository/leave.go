a1bb85f930c217fafeca712714cd3b9fb29ccdee
webbalaka <webbalaka@gmail.com>
2024-05-17 10:15:12 +0700

    add: timezone in db

--- leave-management-service/repository/leave.go
+++ leave-management-service/repository/leave.go
@@ -3,10 +3,10 @@ package repository
 import "time"
 
 type Leave struct {
-	ID         int       `db:"id" json:"id"`
-	EmployeeID string    `db:"employee_id" json:"employee_id"`
-	DateStart  time.Time `db:"date_start" json:"date_start"`
-	DateEnd    time.Time `db:"date_end" json:"date_end"`
-	Reason     string    `db:"reason" json:"reason"`
-	Status     string    `db:"status" json:"status"`
+	ID             int       `db:"id" json:"id"`
+	EmployeeID     string    `db:"employee_id" json:"employee_id"`
+	DateStart      time.Time `db:"date_start" json:"date_start"`
+	DateEnd        time.Time `db:"date_end" json:"date_end"`
+	Reason         string    `db:"reason" json:"reason"`
+	Status         string    `db:"status" json:"status"`
 }
