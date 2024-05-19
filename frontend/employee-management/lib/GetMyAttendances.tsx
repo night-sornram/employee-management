3250cb7c39ed6f36b99bb8b028a2e54a762f591a
night-sornram <6633236021@student.chula.ac.th>
2024-05-19 20:59:00 +0700

    Edit attendance history

--- frontend/employee-management/lib/GetMyAttendances.tsx
+++ frontend/employee-management/lib/GetMyAttendances.tsx
@@ -1,8 +1,10 @@
 export default async function GetMyAttendances (eid: string, token: string) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_ATTENDANCE_URL}/me/${eid}`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_ATTENDANCE_URL}/attendance/me/${eid}`, {
         method: 'GET',
+        mode: 'cors',
         headers: {
             authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
         },
     });
     if (!response.ok) {
--- frontend/employee-management/lib/GetMyLeaves.tsx
+++ frontend/employee-management/lib/GetMyLeaves.tsx
@@ -1,8 +1,10 @@
 export default async function GetMyLeaves (eid: string, token: string) {
     const response = await fetch(`${process.env.NEXT_PUBLIC_LEAVE_URL}/leaves/me/${eid}`, {
         method: 'GET',
+        mode: 'cors',
         headers: {
             authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
         },
     });
     if (!response.ok) {
