f761541c6e290994349fd556cf74fdf96a2bf6d2
unknown <jasonchawsuan@gmail.com>
2024-05-26 08:31:11 +0700

    24/05/24 before present

--- frontend/employee-management/lib/CreateAttendance.tsx
+++ frontend/employee-management/lib/CreateAttendance.tsx
@@ -1,5 +1,5 @@
 export default async function CreateAttendance (token: string, eid:string, dateTime: string, date: string, lid: number) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_ATTENDANCE_URL}/api/attendance`,{
+    const response = await fetch(`${process.env.NEXT_PUBLIC_ATTENDANCE_URL}/api/attendances`,{
         method: "POST",
         mode : "cors",
         headers: {
